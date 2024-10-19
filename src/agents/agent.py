a2c9fbc037712b8861db45c669d2d1499e146a03
valentin-rexer <valentin.rexer@web.de>
2024-10-19 14:12:12 -0500

    implemented first functioning simple bee model

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -1,24 +1,108 @@
 import mesa
 import random
 
+
 class HoneyBeeAgent(mesa.Agent):
+    def __init__(self, bee_agent_id, bee_model, max_nectar):
+        super().__init__(bee_agent_id, bee_model)
+        self.status = 's'
+        self.nectar = 0
+        self.max_nectar = max_nectar
+        self.flower = None
 
-    def __init__(self, bee_agent_id, model):
-        super().__init__(bee_agent_id, model)
+    # Search for flowers in the current cell or move randomly
+    def search(self):
+        if self.flower is None:
+            agents_at_pos = self.model.grid.get_cell_list_contents(self.pos)  # Use self.model
+            available_flowers = [agent for agent in agents_at_pos if isinstance(agent, FlowerAgent)]
+            if available_flowers:
+                self.flower = random.choice(available_flowers)
 
-    #todo: search (fly away from the center) in any direction
+            else:
+                # Move randomly if no flower is found
+                new_pos = (self.pos[0] + random.randint(-1, 1), self.pos[1] + random.randint(-1, 1))
+                if not self.model.grid.out_of_bounds(new_pos):
+                    self.model.grid.move_agent(self, new_pos)  # Correct agent movement
 
-    #todo: fly back in the fastest way possible
+        else:
+            if self.pos == self.flower.pos:
+                if self.flower.nectar > 0:
+                    self.nectar += 1
+                    self.flower.nectar -= 1
 
-    #todo: rest and let nectar sit at hive
+            else:
+                x_inc = 0
+                y_inc = 0
 
-    #todo: load itself with nectar
+                if self.pos[0] < self.flower.pos[0]:
+                    x_inc = 1
+
+                elif self.pos[0] > self.flower.pos[0]:
+                    x_inc = -1
+
+                if self.pos[1] < self.flower.pos[1]:
+                    y_inc = 1
+
+                elif self.pos[1] > self.flower.pos[1]:
+                    y_inc = -1
+
+                self.model.grid.move_agent(self, (self.pos[0] + x_inc, self.pos[1] + y_inc))
+
+
+
+    def return_to_hive(self):
+        x_move = 0
+        y_move = 0
+
+        # Navigate back to the hive
+        if self.pos != self.model.hive:
+            if self.pos[0] < self.model.hive[0]:
+                x_move = 1
+            elif self.pos[0] > self.model.hive[0]:
+                x_move = -1
+
+            if self.pos[1] < self.model.hive[1]:
+                y_move = 1
+            elif self.pos[1] > self.model.hive[1]:
+                y_move = -1
+
+        new_pos = (self.pos[0] + x_move, self.pos[1] + y_move)
+        self.model.grid.move_agent(self, new_pos)  # Move agent to new position
+
+    def unload(self):
+        if self.nectar > 0:
+            self.nectar -= 1
+            self.model.hive_stock += 1  # Update the hive stock in the model
+
+    def update_status(self):
+        # Update the agent's status based on nectar and position
+        if self.nectar == self.max_nectar:
+            if self.pos == self.model.hive:
+                self.status = 'u'  # Unloading at the hive
+            else:
+                self.status = 'c'  # Carrying nectar to the hive
+        elif self.nectar > 0 and self.pos == self.model.hive:
+            self.status = 'u'
+        else:
+            self.status = 's'  # Searching for nectar
+
+    def step(self):
+        self.update_status()
+
+        if self.status == 'u':
+            self.unload()
+
+        if self.status == 'c':
+            self.return_to_hive()
+
+        if self.status == 's':
+            self.search()
 
 
 
 class FlowerAgent(mesa.Agent):
-    def __init__(self, flower_agent_id, model):
-        super().__init__(flower_agent_id, model)
+    def __init__(self, flower_agent_id, bee_model):
+        super().__init__(flower_agent_id, bee_model)
         self.nectar = random.randint(0,100)
 
 
--- src/model/model.py
+++ src/model/model.py
@@ -2,15 +2,16 @@ import mesa
 import random
 
 from mesa.time import SimultaneousActivation
-
 from src.agents.agent import FlowerAgent, HoneyBeeAgent
 
 
 class BeeModel(mesa.Model):
-    def __init__(self, height, width, n_flowers):
+    def __init__(self, height, width, n_flowers, n_bees):
         super().__init__()
         self.height = height
         self.width = width
+        self.hive = (round(width/2), round(height/2))
+        self.hive_stock = 0
 
         self.schedule = SimultaneousActivation(self)
 
@@ -23,7 +24,13 @@ class BeeModel(mesa.Model):
             x = random.randint(0, self.width-1)
             y = random.randint(0, self.height-1)
             self.grid.place_agent(flower_agent, (x, y))
-            self.grid.place_agent(HoneyBeeAgent(100, self), (0,0))
+
+        for i in range(n_bees):
+            honey_bee_agent = HoneyBeeAgent(i, self, random.randint(4,8))
+            self.schedule.add(honey_bee_agent)
+
+            self.grid.place_agent(honey_bee_agent, self.hive)
+
 
     def print_grid(self):
         for y in range(self.grid.height):
@@ -31,17 +38,31 @@ class BeeModel(mesa.Model):
             for x in range(self.grid.width):
                 cell_contents = self.grid.get_cell_list_contents([(x, y)])
                 # Check if any of the agents in the cell is a FlowerAgent
-                if any(isinstance(agent, FlowerAgent) for agent in cell_contents):
+                if (x,y) == self.hive:
+                    row += "H "
+
+                elif any(isinstance(agent, FlowerAgent) for agent in cell_contents):
                     row += "X "
 
                 elif any(isinstance(agent, HoneyBeeAgent) for agent in cell_contents):
-                    row += "H "
+                    row += "_ "
                     
                 else:
                     row += "0 "
             print(row)
 
 
-model = BeeModel(20, 20, 50)
 
-model.print_grid()
+
+values = [50, 200, 500, 2000, 5000, 50000]
+for f in range(10):
+    print(f)
+    print("")
+    for k in values:
+        model = BeeModel(2000, 2000, 7500, 2500)
+        for i in range(k):
+            model.schedule.step()
+
+        print(f'{k}: {model.hive_stock}')
+
+    print("")
