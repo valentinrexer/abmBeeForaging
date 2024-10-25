6817c9a3748a7a3b98c945782e69d9e5baf06c89
valentin-rexer <valentin.rexer@web.de>
2024-10-25 17:20:10 -0500

    created a first anticipation method; still to test

--- src/model/playground.py
+++ src/model/playground.py
@@ -1,7 +1,34 @@
 import mesa
 import grid
-from src.model.grid import CircularGrid
 
-my_grid = CircularGrid(21,21, torus=False)
+
+def anticipation(method, , time_source_found, days_of_experience, sunrise, sunset):
+    if method == 1:
+        return time_source_found
+
+    elif method == 2:
+        return time_source_found - 4 * (time_source_found - sunrise) / (sunset - sunrise)
+
+    elif method == 3 or 4:
+        subtract_anticipation = [3600 * 2, 3600 * 1.5, 3600]
+        initial_anticipation = time_source_found - subtract_anticipation[days_of_experience]
+
+        if method == 4:
+            initial_anticipation -= subtract_anticipation[days_of_experience]
+
+        if initial_anticipation < sunrise:
+            return sunrise
+
+        else:
+            return initial_anticipation
+
+
+    elif method == 5:
+        subtract_factor = [4, 3, 2]
+        return time_source_found - ((time_source_found - sunrise) * (subtract_factor[days_of_experience] / (sunset-sunrise)/3600))
+
+
+
+def clustering_time()
 
 
